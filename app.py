e34169be820357059dbd2de0ecbbff4287f0c3ac
malikmoneyT <95098139+malikmoneyT@users.noreply.github.com>
2024-07-02 00:53:01 -0400

    Create app.py

---
+++ app.py
@@ -0,0 +1,21 @@
+from flask import Flask, render_template, request, jsonify
+from comprehend_service import ComprehendService
+
+app = Flask(__name__)
+
+AWS_REGION = 'ca-central-1'
+comprehend_service = ComprehendService(AWS_REGION)
+@app.route('/')
+def index():
+    return render_template('index.html')
+@app.route('/analyze', methods=['POST'])
+def analyze():
+    text = request.form['text']
+    if not text:
+        return jsonify({'error': 'No text provided'}), 400
+    result = comprehend_service.analyze_sentiment(text)
+    sentiment = result['SentimentScore']
+    return jsonify(sentiment)
+
+if __name__ == '__main__':
+    app.run(debug=True)
